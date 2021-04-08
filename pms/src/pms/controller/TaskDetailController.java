a2a18d1614de57904c0795129ab4352cefafeb2b
ichaeeun <codechaeeun@gmail.com>
2021-04-08 13:45:58 +0900

    태스크수정, comment 추가

--- pms/src/pms/controller/TaskDetailController.java
+++ pms/src/pms/controller/TaskDetailController.java
@@ -1,12 +1,17 @@
 package pms.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import pms.dto.Comment;
+import pms.dto.Task;
 import pms.service.TaskDetailService;
 
 @Controller
@@ -19,7 +24,45 @@ public class TaskDetailController {
 	@GetMapping(params="method=list")
 	public String list(@RequestParam("task_no") int task_no, Model d) {
 		d.addAttribute("detail",service.taskDetail(task_no));
+		d.addAttribute("subdetail",service.subtaskList(task_no));
+		d.addAttribute("taskcurrval",service.taskcurrval());
+		d.addAttribute("comment",service.commentList());
 		return "task_detail";
+	}
+	// http://localhost:8080/pms/taskdetail.do?method=insert
+	@RequestMapping(params="method=insert")
+	public String insertSubtask(Task t,Model d) {
+		System.out.println(t.getTask_name());
+		service.insertSubtask(t);
+		d.addAttribute("success","Y");
+		return "pageJsonReport";
+	}
+	
+	@RequestMapping(params="method=update")
+	public String updateTask(Task t,Model d) {
+		service.updateTask(t);
+		d.addAttribute("success","Y");
+		return "pageJsonReport";
+	}
+	
+	// taskdetail.do?method=updateSub
+	@RequestMapping(params="method=updateSub")
+	public String updateSub(Task t,Model d) {
+		service.updateSub(t);
+		d.addAttribute("success","Y");
+		return "forward:/taskdetail.do?method=list";
+	}
+	
+	@RequestMapping(params="method=insertComment")
+	public String insertComment(Comment com,Model d) {
+		service.insertComment(com);
+		d.addAttribute("success","Y");
+		return "pageJsonReport";
+	}
+	
+	@ModelAttribute("showMember")
+	public ArrayList<Task> showMember(@RequestParam("task_no") int task_no){
+		return service.showMember(task_no);
 	}
 	
 }
--- pms/src/pms/service/TaskDetailService.java
+++ pms/src/pms/service/TaskDetailService.java
@@ -1,9 +1,12 @@
 package pms.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pms.dao.TaskDetailDao;
+import pms.dto.Comment;
 import pms.dto.Task;
 
 @Service
@@ -14,6 +17,38 @@ public class TaskDetailService {
 	
 	public Task taskDetail(int task_no) {
 		return dao.taskDetail(task_no);
+	}
+	
+	public ArrayList<Task> subtaskList(int task_no){
+		return dao.subtaskList(task_no);
+	};
+	
+	public int taskcurrval() {
+		return dao.taskcurrval();
+	}
+	
+	public void insertSubtask(Task t) {
+		dao.insertSubtask(t);
+	}
+	
+	public void updateTask(Task t) {
+		dao.updateTask(t);
+	}
+	
+	public void updateSub(Task t) {
+		dao.updateSub(t);
+	}
+	
+	public ArrayList<Task> showMember(int task_no){
+		return dao.showMember(task_no);
+	}
+	
+	public ArrayList<Comment> commentList(){
+		return dao.commentList();
+	}
+	
+	public void insertComment(Comment com) {
+		dao.insertComment(com);
 	}
 	
 }
